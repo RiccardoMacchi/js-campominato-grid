f6bc3c2e8d3beca64f04a8e2d0adb03861a4e299
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-13 17:47:31 +0200

    generati blocchi dinamicamnte con js

---
+++ js/function.js
@@ -0,0 +1,7 @@
+// funzione creazione element
+
+function newElCl(tag,classAdd){
+    let myBox = document.createElement(tag)
+    myBox.className = classAdd
+    return myBox
+}
---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// Creazione ciclo per genrare 100 blocchi
+for (i=0; i < 100; i++){
+    // Recupero container in pagina
+    const container = document.getElementById("container")
+    // Generazione square in pagina
+    let divSquare = newElCl("div","square")
+    container.append(divSquare)
+}
+
