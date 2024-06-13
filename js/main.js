ac9e1905e2f86ec030f8f95525ed110cb89dfc3e
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-13 18:14:32 +0200

    aggiunto effetto click per inizio gioco e per aggiunta classe azzurro

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
+// Recupero bottone generazione gioco
+const gameStart = document.getElementById("click_me");
+ // Recupero container in pagina
+ const container = document.getElementById("container")
 
-// Creazione ciclo per genrare 100 blocchi
-for (i=0; i < 100; i++){
-    // Recupero container in pagina
-    const container = document.getElementById("container")
-    // Generazione square in pagina
-    let divSquare = newElCl("div","square")
-    container.append(divSquare)
-}
+gameStart.addEventListener("click",
+    ()=>{
+        // Creazione ciclo per genrare 100 blocchi
+        for (i=1; i < 101; i++){
+            // Generazione square in pagina
+            let divSquare = newElCl("div","square")
+            divSquare.innerHTML = `<span>${i}</span>`
+            container.append(divSquare)
+
+            // Cambio classe al click
+            divSquare.addEventListener("click",
+                ()=> {
+                    divSquare.classList.toggle("azzur")
+                }
+            )
+        }
+    }
+)
+
+
+
+
+
 
