281b85764d17c2aa1077eeffd938999904cffc2d
RiccardoMacchi <rick.mac97@outlook.com>
2024-10-09 15:53:29 +0200

    fixed class and resotored select

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ const container = document.getElementById("container")
 
 
 gameStart.addEventListener("click",
-    ()=>{
+    () => {
         // Per far si che il bottone mi cancelli quello che ha appena generato e in caso di duble click lo rigeneri
         container.innerHTML = "";
         // Selezione difficoltà
         const difficult = document.getElementById("select_level").value
         console.log(difficult)
         let numBlock;
-        if (difficult === "cento"){
+        if (difficult === "cento") {
             numBlock = 101
         } else if (difficult === "ottantuno") {
             numBlock = 82
@@ -22,17 +22,24 @@ gameStart.addEventListener("click",
             numBlock = 50
         }
 
-        // Creazione ciclo per generare 100 blocchi
-        for (let i = 1; i < numBlock; i++){
+        // Creazione ciclo per generare blocchi in basse alla difficoltà
+        for (let i = 1; i < numBlock; i++) {
             // Generazione square in pagina
-            let divSquare = newElCl("div","square")
+            let divSquare = newElCl("div", "square")
             divSquare.innerHTML = `<span>${i}</span>`
-            divSquare.classList.add(difficult)
+            // Aggiunta classe personalizzata per livelli
+            if (numBlock === 101) {
+                divSquare.classList.add("cento")
+            } else if (numBlock === 82) {
+                divSquare.classList.add("ottantuno")
+            } else {
+                divSquare.classList.add("quarantanove")
+            }
             container.append(divSquare)
 
             // Cambio classe al click
             divSquare.addEventListener("click",
-                ()=> {
+                () => {
                     divSquare.classList.toggle("azzur")
                     console.log(i)
                 }
